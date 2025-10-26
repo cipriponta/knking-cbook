3d3090000fc1bf091b68823cff9a192076b34287
cipriponta <ponta_ciprian@yahoo.com>
2025-10-26 18:56:09 +0200

    [C3] Exercises from C3

---
+++ c3/examples/e2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World with bell\a\n");
+    fflush(stdout);
+    return 0;
+}
---
+++ c3/exercises/e2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float x = 123.4567;
+    printf("%-8.1e\n", x);
+    printf("%10.6e\n", x);
+    printf("%-8.3f\n", x);
+    printf("%6.0f\n", x);
+
+    return 0;
+}
